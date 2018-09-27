4591034ec517b31717c2365401dea9d7aa822836
decagondev <decagondevelopment@gmail.com>
2018-09-27 18:21:51 +0100

    (STRETCH : LOGIN) add skeleton for the Auth component

---
+++ lambda-notes/src/components/Authentication/Auth/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Login from '../SignIn';
+
+const Auth = App =>
+class extends React.Component {
+  state = {
+    autenticated: false,
+    username: ""
+  }
+
+  componentDidMount() {}
+
+  render() {}
+};
+
+export default Auth;
