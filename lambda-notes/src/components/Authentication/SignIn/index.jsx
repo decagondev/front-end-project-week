d7701f67d200f3aafbafd01ab9f435ddb358f3f5
decagondev <decagondevelopment@gmail.com>
2018-09-27 17:25:39 +0100

    (STRETCH : LOGIN) add state for the signin component

--- lambda-notes/src/components/Authentication/SignIn/index.jsx
+++ lambda-notes/src/components/Authentication/SignIn/index.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 
 export default class SignIn extends Component {
+  
+  state = {
+    username: "",
+    password: ""
+  };
+  
   render() {
     return (
       <div>
